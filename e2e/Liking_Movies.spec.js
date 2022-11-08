cc62144b7ba29b8e7b49823690984c7d671ecc3a
arialghifari <arialghifari99@gmail.com>
2022-11-08 09:58:12 +0800

    E2E Searching movies

--- e2e/Liking_Movies.spec.js
+++ e2e/Liking_Movies.spec.js
@@ -9,14 +9,14 @@ Before(({ I }) => {
 
 Scenario('showing empty liked movies', ({ I }) => {
   I.seeElement('#query');
-  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+  I.waitForElement('.movie-item__not__found');
 });
 
 Scenario('liking one movie', async ({ I }) => {
-  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+  I.waitForElement('.movie-item__not__found');
   I.amOnPage('/');
 
-  I.waitForElement('.movie__title a');
+  I.waitForElement('.movie__title a', 10);
 
   const firstFilm = locate('.movie__title a').first();
   const firstFilmTitle = await I.grabTextFrom(firstFilm);
@@ -31,4 +31,41 @@ Scenario('liking one movie', async ({ I }) => {
   const likedFilmTitle = await I.grabTextFrom('.movie__title');
 
   assert.strictEqual(firstFilmTitle, likedFilmTitle);
+});
+
+Scenario('searching movies', async ({ I }) => {
+  I.waitForElement('.movie-item__not__found');
+  I.amOnPage('/');
+
+  I.waitForElement('.movie__title a', 10);
+  const titles = [];
+
+  for (let i = 1; i <= 3; i++) {
+    I.click(locate('.movie__title a').at(i));
+    I.seeElement('#likeButton');
+    I.click('#likeButton');
+    titles.push(await I.grabTextFrom('.movie__title'));
+    I.amOnPage('/');
+  }
+
+  I.amOnPage('/#/like');
+  I.seeElement('#query');
+
+  const searchQuery = titles[1].substring(1, 3);
+  const matchingMovies = titles.filter(
+    (title) => title.indexOf(searchQuery) !== -1
+  );
+
+  I.fillField('#query', searchQuery);
+  I.pressKey('Enter');
+
+  const visibleLikedMovies = await I.grabNumberOfVisibleElements('.movie-item');
+  assert.strictEqual(matchingMovies.length, visibleLikedMovies);
+
+  matchingMovies.forEach(async (title, index) => {
+    const visibleTitle = await I.grabTextFrom(
+      locate('.movie__title').at(index + 1)
+    );
+    assert.strictEqual(title, visibleTitle);
+  });
 });

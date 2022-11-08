205c9ad54808cb93836a222f0fc0f586022f6f5b
arialghifari <arialghifari99@gmail.com>
2022-11-08 08:25:59 +0800

    E2E Liking one movie

--- e2e/Liking_Movies.spec.js
+++ e2e/Liking_Movies.spec.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 /* eslint-disable no-undef */
 Feature('Liking Movies');
 
@@ -10,8 +12,23 @@ Scenario('showing empty liked movies', ({ I }) => {
   I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
 });
 
-Scenario('liking one movie', ({ I }) => {
+Scenario('liking one movie', async ({ I }) => {
   I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
   I.amOnPage('/');
-  // next test
+
+  I.waitForElement('.movie__title a');
+
+  const firstFilm = locate('.movie__title a').first();
+  const firstFilmTitle = await I.grabTextFrom(firstFilm);
+  I.click(firstFilm);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('.movie-item');
+
+  const likedFilmTitle = await I.grabTextFrom('.movie__title');
+
+  assert.strictEqual(firstFilmTitle, likedFilmTitle);
 });
